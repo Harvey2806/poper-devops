4db127a4ba5b1e5b06e8365130e4b1380dc1a7f6
Harvey Zheng <harvey.zheng@activenetwork.com>
2024-07-21 13:15:24 +0800

    rewrite csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Http\Middleware;
+use Closure;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
@@ -13,4 +14,9 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
       '*',
     ];
+
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
 }
